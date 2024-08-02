f934ff0d359367f1340002a2929040d4f066fe38
Vinayak2k03 <vinayaknagar2810@gmail.com>
2024-08-02 15:52:59 +0530

    random commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 function Footer(){
     return(
     <div className="bg-gray-500 flex justify-between px-8 py-4">
-        <p className="text-white">Copyright 2024 | CodeYogi</p>
+        <p className="text-white">Copyright 2024  |  CodeYogi</p>
         <p className="text-white">Powered by CodeYogi</p>
 
       </div>
